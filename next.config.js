337e95e4fe440fc6f994dd46af18b47fc9494e50
Akash <151770412+akashgoud2121@users.noreply.github.com>
2025-08-13 17:25:38 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,30 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-  webpack(config, { isServer }) {
-    // Ensure the CSS modules and PostCSS are processed correctly
+  reactStrictMode: true,
+  webpack(config) {
     config.module.rules.push({
       test: /\.css$/,
-      use: [
-        'style-loader', 
-        'css-loader', 
-        'postcss-loader'
-      ],
+      use: ['style-loader', 'css-loader', 'postcss-loader'],
     });
-
     return config;
   },
 };
